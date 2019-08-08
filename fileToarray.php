b3f37386ec26046de69f331afefabf0366ab3536
Erick <Gehjaniaina@gmail.com>
2019-08-08 14:05:01 +0300

    mettre des commentaires dans cette ligne de code

--- fileToarray.php
+++ fileToarray.php
@@ -21,29 +21,29 @@ $res = explode("\;", $file,2);//vehicule sans moteur... et avec moteur...
 
 $array1 = [];
 array_push($array1, $resA2);
-$final = array_fill_keys($array1, $resA_1);
+$final = array_fill_keys($array1, $resA_1);//LASA CLE ILAY SANS MOTEUR
 			
 
 	$resB = explode(">", $res[1],2); //Avec  moteur et ......
 		$resB_1 = explode(";", $resB[1],3);//voiture et camion et car
 		$resB1 = preg_replace ('#[^.0-9a-z]+#i', ' ', $resB[0]);//Avec moteur
 	$array = [];
-	array_push($array, $resB1);
-	$finale = array_fill_keys($array, $resB_1);
+	array_push($array, $resB1);//AVADIKA ARRAY LAY VALEUR AVEC ARRAY
+	$finale = array_fill_keys($array, $resB_1);//AVADIKA CLE ANLAY voiture camion et car ILAY VALEUR "AVEC MOTEUR"
 			
 $vehicule = [];
-$resfilter = preg_replace ('#[^0-9a-z]+#i',' ',$resA[0]);
-array_push($vehicule, $resfilter);
-$sommeArray = array_merge($finale,$final);
-$array_vehicule = array_fill_keys($vehicule,$sommeArray);
+$resfilter = preg_replace ('#[^0-9a-z]+#i',' ',$resA[0]);//MANALA ANREO CARACTERE SPECIAUX REETRA
+array_push($vehicule, $resfilter);//POUSSENA ANATY VEHICULE AV EO
+$sommeArray = array_merge($finale,$final);//AKAMBANA HO LASA ARRAY 1 LE ARRAY 2 REO
+$array_vehicule = array_fill_keys($vehicule,$sommeArray);//POUSSENA ANATY VEHICULE HO LASA CLE ANANKIROA Avec MOTEUR sy SANS MOTEUR
 
-foreach ($array_vehicule as $key => $value) {
+foreach ($array_vehicule as $key => $value) {//MAKA ANLAY CLE VEHICULE FOTSINY
 	echo '<h2>'.$key.'</h2>';
 	ksort($value);
-		foreach ($value as $key => $value1) {
+		foreach ($value as $key => $value1) {//MAKA ANLAY CLE AVEC ET SANS MOTEUR MAIS 2SONT VALEUR DE CLE VEHICULE
 			echo '<b>'.$key.'</b>'.'<br>';
 			asort($value1);
-			foreach ($value1 as $key => $value2) {
+			foreach ($value1 as $key => $value2) {//MAKA ANLAY VALEUR ANATY CLE ANANKIROA
 				$rez = preg_replace ('#[^0-9a-z]+#i',' ',$value2);
 				echo $rez.'<br>';
 			}
